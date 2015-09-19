7ced6b3f10085cb9ee69fa53e36923c17218bf12
Evelyn Wang <wangtianlu0709@163.com>
2015-09-19 11:12:34 -0400

    fix relative path and delete cd command

--- example/train_test.sh
+++ example/train_test.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-cd ../
-
-<<<<<<< HEAD
- rm -r $MODEL_DIR
-
-/Users/koala/Documents/lab/Blender/LORELEI/active_learning/ne-tagger/train.py -S $TRAIN_SCP $MODEL_DIR $LTF_DIR
-/Users/koala/Documents/lab/Blender/LORELEI/active_learning/ne-tagger/tagger.py -S $TEST_SCP -L $SYS_LAF_DIR $MODEL_DIR
-#/Users/koala/Documents/lab/Blender/LORELEI/active_learning/ne-tagger/tagger.py -S $TEST_SCP -L $SYS_LAF_DIR /Users/koala/Documents/lab/Blender/LORELEI/active_learning/ne-tagger/hausa_ne_model
-
-/Users/koala/Documents/lab/Blender/LORELEI/active_learning/ne-tagger/score.py $REF_LAF_DIR $SYS_LAF_DIR $LTF_DIR
-=======
 MODEL_DIR=./example/model      # directory for trained model
 LTF_DIR=./example/ltf          # directory containing LTF files
 SYS_LAF_DIR=./example/output   # directory for tagger output (LAF files)
@@ -20,9 +9,9 @@ REF_LAF_DIR=./example/laf      # directory containing gold standard LAF files
 
 #rm -r $MODEL_DIR
 
-#./train.py -S $TRAIN_SCP $MODEL_DIR $LTF_DIR
-./tagger.py -S $TEST_SCP -L $SYS_LAF_DIR $MODEL_DIR
-# ./tagger.py -S $TEST_SCP -L $SYS_LAF_DIR ..//hausa_ne_model
+./train.py -S $TRAIN_SCP $MODEL_DIR $LTF_DIR
+#./tagger.py -S $TEST_SCP -L $SYS_LAF_DIR $MODEL_DIR
+./tagger.py -S $TEST_SCP -L $SYS_LAF_DIR ./hausa_ne_model
 
-#./score.py $REF_LAF_DIR $SYS_LAF_DIR $LTF_DIR
->>>>>>> 023ade4b4a7e6ef1f261e828c4637dd3f034ae34
+./score.py $REF_LAF_DIR $SYS_LAF_DIR $LTF_DIR
+
